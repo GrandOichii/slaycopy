423b99e1d8dbdc8795fae7ab2320b7ebabd62f6f
GrandOichii <i.hawk@yandex.ru>
2023-04-25 14:43:56 +0400

    Added separate tile class, moved some map data to config file, started work on town spawn points

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import pygame as pg
-
+import math
 # pg.init()
 
 # screen = pg.s
@@ -12,6 +12,18 @@ import random
 HEIGHT = 25
 WIDTH = 18
 WINDOW = None
+
+class Tile:
+    def __init__(self, y, x):
+        self.y = y
+        self.x = x
+        self.color_pair = 0
+
+        self.solid = False
+
+
+HIGHTLIGHT_POINTS = []
+
 
 CORRECTIONS = [
     {
@@ -75,62 +87,35 @@ CORRECTIONS = [
 ]
 
 
-def matches(map, template, posy, posx):
-    for i in range(len(template)):
-        line = template[i]
-        for j in range(len(line)):
-            y = i + posy
-            if y >= len(map):
-                return False
-            x = j + posx
-            if x >= len(map[0]):
-                return False
-            if line[j] != map[y][x]:
-                return False
-    return True
+class MapGenerationConfig:
+    def __init__(self):
+        self.height = 10
+        self.width = 10
 
 
-def reverse_matches(map, template, posy, posx):
-    newt = []
-    for line in template:
-        newt += [line[::-1]]
-    return matches(map, newt, posy, posx)
+def generate_map(config: MapGenerationConfig=None) -> tuple[list[list[Tile]], list[tuple[int, int]]]:
+    if not config:
+        config = MapGenerationConfig()
 
+    result: list[list[Tile]] = []
+    coords = []
 
-def replace(map, correction, posy, posx):
-    for i in range(len(correction)):
-        line = correction[i]
-        for j in range(len(line)):
-            y = i + posy
-            x = j + posx
-            map[y][x] = line[j]
-
-
-def reverse_replace(map, correction, posy, posx):
-    newt = []
-    for line in correction:
-        newt += [line[::-1]]
-    replace(map, newt, posy, posx)
-
-
-def generate_map() -> list[list]:
-    result = []
-
-    for i in range(HEIGHT):
+    for i in range(config.height):
         a = []
-        for j in range(WIDTH):
-            a += [False]
+        for j in range(config.width):
+            a += [Tile(i, j)]
         result += [a]
 
     # generate start point
-    offset_y = HEIGHT // 3
-    offset_x = WIDTH // 3
+    # offset_y = config.height // 3
+    # offset_x = config.width // 3
     start_point_loc = [
-        random.randint(offset_y, HEIGHT-offset_y),
-        random.randint(offset_x, WIDTH-offset_x),
+        config.height // 2,
+        config.width // 2
     ]
 
-    result[start_point_loc[0]][start_point_loc[1]] = True
+    result[start_point_loc[0]][start_point_loc[1]].solid = True
+    coords += [(start_point_loc[0], start_point_loc[1])]
     step_weight = 0.01
 
     offshoots_count = 3
@@ -166,19 +151,22 @@ def generate_map() -> list[list]:
             newy = offshoot[0] + dir[0]
             newx = offshoot[1] + dir[1]
             
-            if not (newy < 0 or newx < 0 or newy >= HEIGHT or newx >= WIDTH):
+            if not (newy < 0 or newx < 0 or newy >= config.height or newx >= config.width):
                 offshoot[0] = newy
                 offshoot[1] = newx
-                if not result[offshoot[0]][offshoot[1]]:
+
+                if not result[offshoot[0]][offshoot[1]].solid:
                     count += 1
-                result[offshoot[0]][offshoot[1]] = True
+                result[offshoot[0]][offshoot[1]].solid = True
+                coords += [(offshoot[0], offshoot[1])]
+
             if count > count_thresh and random.randint(0, 100) < offshoot[2] * step_weight:
                 offshoots.remove(offshoot)
-        draw_map(WINDOW, result, 5, [0,0])
-        WINDOW.refresh()
-        # WINDOW.getch()
-        curses.napms(20)
-    curses.flash()
+    #     draw_map(WINDOW, result, 5, [0,0])
+    #     WINDOW.refresh()
+    #     # WINDOW.getch()
+    #     curses.napms(20)
+    # curses.flash()
 
     # correct missing pieces
     # for correction in CORRECTIONS:
@@ -205,19 +193,19 @@ def generate_map() -> list[list]:
     # cut the borders
     # border_width = 2
     # for bi in range(border_width):
-    #     for i in range(bi, HEIGHT - bi):
+    #     for i in range(bi, config.height - bi):
     #         result[i][bi] = False
-    #         result[i][WIDTH - 1 - bi] = False
-    #     for j in range(bi, WIDTH - bi):
+    #         result[i][config.width - 1 - bi] = False
+    #     for j in range(bi, config.width - bi):
     #         result[bi][j] = False
-    #         result[HEIGHT - 1 - bi][j] = False
+    #         result[config.height - 1 - bi][j] = False
     # for i in range(len(result)):
     #     line = result[i]
     #     for j in range(len(line)):
     #         if ( or ) and ( or ):
     #             result[i][j] = False
 
-    return result
+    return result, coords
 
 
 SPRITE = [
@@ -240,13 +228,14 @@ def defer_draw(func, args):
     }]
 
 
-def draw_selected(args):
+def draw_with_color_pair(args):
     win = args['win']
     y = args['y']
     x = args['x']
-    win.attron(curses.color_pair(1))
+    color_pair = args['color_pair']
+    win.attron(curses.color_pair(color_pair))
     basic_draw_sprite(win, y, x)
-    win.attroff(curses.color_pair(1))
+    win.attroff(curses.color_pair(color_pair))
 
 
 def basic_draw_sprite(win, y, x):
@@ -254,34 +243,42 @@ def basic_draw_sprite(win, y, x):
         win.addstr(y + ii, x, SPRITE[ii])
 
 
-def draw_sprite(win, y, x, sprite_info):
-    if not sprite_info['tile']:
-        return
-    basic_draw_sprite(win, y, x)
-    if sprite_info['tile']:
-        win.addstr(y + 1, x + 2, f'aa')
-        win.addstr(y + 1, x + 2, str(sprite_info['pos'][0]))
-        win.addstr(y + 2, x + 2, 'aa')
-        win.addstr(y + 2, x + 2, str(sprite_info['pos'][1]))
-        win.addstr(y + 3, x + 2, 'aa')
-
-    if not sprite_info['selected']:
+def draw_sprite(win, y, x, tile, selected):
+    if not tile.solid:
         return
     
-    defer_draw(draw_selected, {'win': win, 'y': y, 'x': x})
+    # win.attron(curses.color_pair(tile.color_pair))
+    basic_draw_sprite(win, y, x)
+    win.addstr(y + 1, x + 2, f'aa')
+    # win.addstr(y + 1, x + 2, str(tile['pos'][0]))
+    win.addstr(y + 2, x + 2, 'aa')
+    # win.addstr(y + 2, x + 2, str(tile['pos'][1]))
+    win.addstr(y + 3, x + 2, 'aa')
+    if tile.color_pair != 0:
+        defer_draw(draw_with_color_pair, {'win': win, 'y': y, 'x': x, 'color_pair': tile.color_pair})
+    # win.attroff(curses.color_pair(tile.color_pair))
+
+
+    if not selected:
+        return
+    
+    defer_draw(draw_with_color_pair, {'win': win, 'y': y, 'x': x, 'color_pair': 1})
 
 
 def draw_map(win, map, xbase, pos):
     y = 0
     x = 0
     xdiff = xbase
-    for i in range(HEIGHT):
+    height = len(map)
+    width = len(map[0])
+    for i in range(height):
         x = xdiff
-        for j in range(WIDTH):
-            draw_sprite(win, y, x, {'tile': map[i][j], 'selected': pos[0] == i and pos[1] == j, 'pos': [i, j]})
+        for j in range(width):
+            draw_sprite(win, y, x, map[i][j], pos[0] == i and pos[1] == j)
             x += len(SPRITE[0]) + 4
         xdiff = xbase - xdiff
         y += len(SPRITE) - 3
+
 
 RESULT = {}
 def main(stdscr: 'curses._CursesWindow'):
@@ -289,8 +286,58 @@ def main(stdscr: 'curses._CursesWindow'):
     WINDOW = stdscr
     curses.curs_set(0)
     curses.init_pair(1, curses.COLOR_RED, 0)
-    map = generate_map()
-    RESULT['map'] = map
+    curses.init_pair(2, curses.COLOR_BLUE, 0)
+    curses.init_pair(3, curses.COLOR_RED, 0)
+    curses.init_pair(4, curses.COLOR_YELLOW, 0)
+    curses.init_pair(5, curses.COLOR_MAGENTA, 0)
+    curses.init_pair(6, curses.COLOR_GREEN, 0)
+    config = MapGenerationConfig()
+    config.height = HEIGHT
+    config.width = WIDTH
+    game_map, coords = generate_map(config)
+    RESULT['map'] = game_map
+
+    # find center
+    # sumsy = [sum(line) for line in map]
+    # sumsx = []
+    # for j in range(len(map[0])):
+    #     s = 0
+    #     for i in range(len(map)):
+    #         s += map[i][j]
+    #     sumsx += [s]
+    # sumsy += []
+
+    # global CENTER_Y, CENTER_X
+    y = sum([p[0] for p in coords]) // len(coords)
+    x = sum([p[1] for p in coords]) // len(coords)
+
+    # TODO move to center?
+
+    game_map[y][x].color_pair = 2
+
+    num_of_players = 4
+    angle = random.randint(0, 6)
+    for playeri in range(num_of_players):
+        yloc = y
+        xloc = x
+        ydiff = math.sin(angle)
+        xdiff = math.cos(angle)
+        last_loc = (yloc, xloc)
+        while True:
+            t = game_map[int(yloc)][int(xloc)]
+            if t.solid:
+                last_loc = (int(yloc), int(xloc))
+                        
+            yloc += ydiff
+            xloc += xdiff
+            if yloc < 0 or xloc < 0 or yloc >= config.height or xloc >= config.width:
+                break
+        t = game_map[last_loc[0]][last_loc[1]]
+        t.color_pair = playeri + 3
+        # spread
+
+        angle += 2 * math.pi / num_of_players
+
 
     pos = [0, 0]
 
@@ -304,9 +351,8 @@ def main(stdscr: 'curses._CursesWindow'):
     while True:
         # draw
         xbase = 5
-        draw_map(stdscr, map, xbase, pos)
-            # y += len(SPRITE)
-        # draw defered
+        draw_map(stdscr, game_map, xbase, pos)
+
         global DEFERED_DRAW
         for defered in DEFERED_DRAW:
             defered['func'](defered['args'])
@@ -322,13 +368,13 @@ def main(stdscr: 'curses._CursesWindow'):
             pos[0] += diff[0]
             if pos[0] < 0:
                 pos[0] = 0
-            if pos[0] >= HEIGHT:
-                pos[0] = HEIGHT - 1
+            if pos[0] >= config.height:
+                pos[0] = config.height - 1
             pos[1] += diff[1]
             if pos[1] < 0:
                 pos[1] = 0
-            if pos[1] >= WIDTH:
-                pos[1] = WIDTH - 1
+            if pos[1] >= config.width:
+                pos[1] = config.width - 1
 
         
         # clear
@@ -338,5 +384,19 @@ curses.wrapper(main)
 rmap = RESULT['map']
 for line in rmap:
     for t in line:
-        print((1 if t else 0), end=' ')
+        print(t.color_pair, end=' ')
     print()
+
+# num_of_players = 3
+# for playeri in range(num_of_players):
+#     angle = 0
+#     yloc = y
+#     xloc = x
+#     for i in range(3):
+#         yloc += math.sin(angle)
+#         xloc += math.cos(angle)
+#         try:
+#             game_map[int(yloc)][int(xloc)].color_pair = playeri + 3
+#         except Exception:
+#             pass
+#     angle += math.pi / num_of_players
